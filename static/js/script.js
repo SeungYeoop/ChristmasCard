deb3e7d9d9de2ee217d7e652c9a37f5dd27c19b2
Seung8521 <seungyeop@letscoding.kr>
2024-12-07 11:50:14 +0900

    스크롤 만들기

--- static/js/script.js
+++ static/js/script.js
@@ -17,4 +17,23 @@ function changeRows() {
     }
 }
 
+function toggleMore(index) {
+    console.log("Toggle more called for index:", index);
+    const message = document.getElementById('message-' + index);
+    const moreBtn = message.nextElementSibling; // 버튼을 찾음
 
+    // 펼쳐졌으면 숨기고, 숨겨졌으면 펼침
+    if (message.style.maxHeight === 'none' || message.style.maxHeight === '') {
+        console.log("Expanding message...");
+        message.style.maxHeight = '4.8em'; // 3줄로 다시 축소
+        message.style.overflow = 'hidden';
+        message.style.textOverflow = 'ellipsis';
+        moreBtn.innerText = '더보기';
+    } else {
+        console.log("Collapsing message...");
+        message.style.maxHeight = 'none'; // 내용 펼침
+        message.style.overflow = 'visible';
+        message.style.textOverflow = 'clip';
+        moreBtn.innerText = '닫기';
+    }
+}
